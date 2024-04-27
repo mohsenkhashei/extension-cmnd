10699ac944939ee1a5199c8127ba673d69bf05ac
Mustafa <mustafaguner235@gmail.com>
2024-04-27 15:51:40 +0300

    #14 - add nodemailer to project to send an email to the personnel that are assigned to task.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+const path = require('path');
+require("dotenv").config({
+  path: path.join(__dirname, '.env')
+});
 const PORT = process.env.PORT || 8000;
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,6 +9,7 @@ const app = express();
 const router = require("./router.js");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1/", router);
 
 
