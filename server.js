8c6abb13c8b463267c68a1a981ebf0f883e7265a
Mustafa <mustafaguner235@gmail.com>
2024-04-27 12:10:17 +0300

    #9 init the project structure as base.

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const router = require("./router.js")
+
+app.use(bodyParser.json());
+app.use('api/v1/', router);
+
+app.listen(PORT, () =>
+  console.log(`server running on PORT http://localhost:${PORT}`)
+);
+
