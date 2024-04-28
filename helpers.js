6abca72541aaca365972a5b607c265577e7b0738
Mustafa <mustafaguner235@gmail.com>
2024-04-28 12:28:39 +0300

    small fixes

--- helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  joinWithOr: function (array) {
-    if (!Array.isArray(array) || array.length === 0) {
-      return "";
-    }
-    return array.join(" or ");
-  },
-};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const path = require("path");
 require("dotenv").config({
-  path: path.join(__dirname, '.env')
+  path: path.join(__dirname, ".env"),
 });
 const PORT = process.env.PORT || 8001;
 const express = require("express");
@@ -12,8 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/v1/", router);
 
-
 app.listen(PORT, () =>
   console.log(`server running on PORT http://localhost:${PORT}`)
 );
-
