e04c5f57706016b7a64b249c518019b7b58f4a4e
Mustafa <mustafaguner235@gmail.com>
2024-04-28 11:31:39 +0300

    add service types as endpoint

---
+++ controllers/ServiceTypeController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  index: function () {
+    return "Room Cleaning or Laundry Service or Room Service or Maintenance Request or Damage Report or Lockout Assistance or Reservation Inquiry or Complaint Resolution or Food Delivery or Technical Support or Housekeeping Request or Transportation Assistance or Front Desk Assistance or Lost and Found Inquiry or Special Event Planning or Emergency Medical Assistance or Internet Connection Issues";
+  },
+};
--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const PersonnelController = require("./controllers/PersonnelController.js");
+const ServiceTypeController = require("./controllers/ServiceTypeController.js");
 
 router.get("/personnel/call/:service_type/:room_id", PersonnelController.call);
 router.get("/claim-task", PersonnelController.claim);
+router.get("/service-types", ServiceTypeController.index);
 
 module.exports = router;
